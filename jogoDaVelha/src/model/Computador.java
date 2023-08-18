6a0eed217b38d967bf7dab4f07701f9943ead0c5
will <126467604+Ifwill@users.noreply.github.com>
2023-08-18 11:04:58 -0300

    Add files via upload

---
+++ jogoDaVelha/src/model/Computador.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Computador extends Jogador {
+	public Computador(int jogador) {
+		super(jogador);
+		System.out.println("Jogador 'Computador' criado!");
+	}
+
+	@Override
+	public void jogar(Tabuleiro tabuleiro) {
+
+	}
+
+	@Override
+	public void Tentativa(Tabuleiro tabuleiro) {
+
+	}
+}
---
+++ jogoDaVelha/src/principal/Main.java
@@ -0,0 +1,10 @@
+package principal;
+
+import model.Jogo;
+
+public class Main {
+	public static void main(String[] args) {
+		Jogo jogo = new Jogo();
+	}
+
+}
